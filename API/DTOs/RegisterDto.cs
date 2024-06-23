85df1ae4d2e0f13d04ce2909e4b96b662266a7ea
nasrulnematix <nasrul@pinc.my>
2024-06-23 13:14:44 +0800

    add API authentication

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterDto
+{
+    [MaxLength(100)]
+    public required string UserName { get; set; }
+
+
+    [MaxLength(100)]
+    public required string Password { get; set; }
+
+}
