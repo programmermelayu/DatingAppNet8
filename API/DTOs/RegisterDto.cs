fc5686b7d9b06cf4828ce3f5c318b2200205b5a6
nasrulnematix <nasrul@pinc.my>
2024-06-24 12:09:22 +0800

    end of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,11 +4,11 @@ namespace API.DTOs;
 
 public class RegisterDto
 {
-    [MaxLength(100)]
-    public required string UserName { get; set; }
+    [Required]
+    public string UserName { get; set; }
 
-
-    [MaxLength(100)]
-    public required string Password { get; set; }
+    [Required]
+    [StringLength(8, MinimumLength =4)]
+    public string Password { get; set; }
 
 }
