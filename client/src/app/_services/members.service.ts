8e5c999e907d9780a59884d0d5b6c51735e419ed
nasrulnematix <nasrul@nematix.com>
2024-07-31 09:58:15 +0800

    End of section 10

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
-
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +10,28 @@ import { Member } from '../_models/member';
 export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
- 
-  getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+  members = signal<Member[]>([]);
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users').subscribe({
+      next: (members) => this.members.set(members),
+    });
   }
 
-  getMember(username: string){
+  getMember(username: string) {
+    const member = this.members().find((x) => x.username === username);
+    if (member !== undefined) return of(member);
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      tap(() => {
+        this.members.update((members) =>
+          members.map((m) => (m.username === member.username ? member : m))
+        );
+      })
+    );
+  }
 }
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,7 +8,8 @@ import { authGuard } from './_guards/auth.guard';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
-
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [authGuard] },
@@ -17,14 +18,19 @@ export const routes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [authGuard],
     children: [
-      { path: 'members', component: MemberListComponent},
-      { path: 'member/:username', component: MemberDetailComponent},
+      { path: 'members', component: MemberListComponent },
+      { path: 'members/:username', component: MemberDetailComponent },
+      {
+        path: 'member/edit',
+        component: MemberEditComponent,
+        canDeactivate: [preventUnsavedChangesGuard],
+      },
       { path: 'lists', component: ListsComponent },
-      { path: 'messages', component: MessagesComponent }
-    ]
+      { path: 'messages', component: MessagesComponent },
+    ],
   },
-  {path: 'errors', component: TestErrorsComponent},
-  {path: 'not-found', component: NotFoundComponent},
-  {path: 'server-error', component: ServerErrorComponent},
+  { path: 'errors', component: TestErrorsComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: 'server-error', component: ServerErrorComponent },
   { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
